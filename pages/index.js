3db50694c41df0a8446ec15145f5ff49d17fb7b8
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2023-03-18 18:29:49 +0330

    navbar finish

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className=''>
       <Head>
         <title>instagram</title>
         <link rel="icon" href="/Logo.ico" />
       </Head>
-      <main className=''>
-        <div className='text-9xl text-red-600'>Hello instagram</div>
-      </main>
+      <Navbar/>
     </div>
   )
 }
